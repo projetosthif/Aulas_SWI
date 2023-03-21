992fe5bda6c8a4450d05655909ca3313085ff032
Thifany Garcia <100360093+projetosthif@users.noreply.github.com>
2023-03-21 12:30:30 -0300

    Add files via upload

---
+++ banco/Conta.cs
@@ -0,0 +1,29 @@
+﻿namespace banco;
+class Conta
+{
+    public int Numero {get; set;}
+    private double Saldo {get; set;}
+    public double Limite {get; set;}
+
+    public void depositar (double valor){
+        this.Saldo += valor;
+    }
+
+     public void sacar (double valor){
+        if (valor> this.Saldo + this.Limite){
+            Console.WriteLine("Você não pode fazer este saque");
+        }else{
+            this.Saldo -= valor;
+        }
+        this.Saldo = valor;
+    }
+    public double MostrarSaldo(){
+        return this.Saldo + this.Limite;
+    }
+
+    public void AjustarLimite(double valor){
+        this.Limite = valor;
+    }
+
+    
+}
---
+++ banco/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace banco;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Banco etec MCM");
+        Conta c = new Conta();
+        // c.Saldo = 1500;
+        c.depositar(1500);
+        //c.Limite = 500;
+        c.AjustarLimite(500);
+
+        
+
+        
+
+       // Console.WriteLine("Seu saldo é de: " + c.Saldo + ", e seu limite é de: " + c.Limite);
+        Console.WriteLine("Seu saldo é de: " + c.MostrarSaldo());
+}
+
+}
