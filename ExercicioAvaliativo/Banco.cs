ff847cdc63d41afbf18f682aa08fac4113c8ea7a
Thifany Garcia <100360093+projetosthif@users.noreply.github.com>
2023-05-08 08:03:31 -0300

    Add files via upload

---
+++ ExercicioAvaliativo/Banco.cs
@@ -0,0 +1,45 @@
+﻿namespace ExercicioAvaliativo;
+class Banco
+{   
+    public int Numero {get; set;}
+    private double Saldo { get;  set;}
+    public double Limite {get; private set;}
+
+    public void AjusteLimite(double valor){
+        this.Limite = valor;
+    }
+    public void Deposito( double valor){
+        this.Saldo += valor;
+    }
+    public void Saque(double valor){
+        if (this.Saldo + this.Limite < valor){
+            Console.WriteLine("Você não tem saldo suficiente.");
+        }else{
+             this.Saldo -= valor;
+        }
+    }
+    public double MostrarSaldo(){
+        return this.Saldo + this.Limite;
+    }
+
+    public double parcela {get; set;}
+    public double valor {get; set;}
+
+    public string emprestimo(int parcelas, double dinheiro){
+        this.parcela = parcelas;
+        this.valor = dinheiro;
+        
+        double divi = dinheiro/parcelas;
+        double multi = dinheiro * 0.06;
+        double soma = multi + divi;
+        double total = soma * parcelas;
+
+        string resultado = "O valor da parcela sem juros é de: R$"+ divi +". O valor da parcela com juros é de: R$"+ multi + ". O juros com prestações é de: R$"+ soma + " O valor total a pagar é de: R$"+total;
+        return resultado;
+        
+     
+    }
+    public void imprimir(string texto){
+        Console.WriteLine(texto);
+    }
+}
---
+++ ExercicioAvaliativo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioAvaliativo;
+class Program
+{
+    static void Main(string[]args)
+    {
+        Banco b = new Banco();
+
+        b.Deposito(1000);
+        b.Saque(1600);
+        b.AjusteLimite(500);
+
+        double valortotal = b.MostrarSaldo();
+
+        Console.WriteLine("O seu saldo  é de " + valortotal);
+        
+        b.imprimir(b.emprestimo(12,1000));
+    }
+}
